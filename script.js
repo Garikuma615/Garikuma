87326a8d75b96655174f12367420a920643b4115
Garikuma615 <garigarinokuma@gmail.com>
2026-02-12 20:01:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var colors = new Array(
+  [62,35,255],
+  [60,255,60],
+  [255,35,98],
+  [45,175,230],
+  [255,0,255],
+  [255,128,0]);
+
+var step = 0;
+//color table indices for: 
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
+
